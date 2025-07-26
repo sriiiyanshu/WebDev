41cf86f4a476010d2729aeaab9fb55e3d665b434
Sriyanshu <sriiiyanshu@gmail.com>
2025-07-26 14:06:46 +0530

    node js

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// import generateName from "sillyname";
+
+// var sillyname = generateName();
+
+// console.log(`My silly name is ${sillyname}.`);
+
+import {randomSuperhero} from "superheroes";
+
+var myName = randomSuperhero();
+
+console.log(`I am ${myName}`);
